800c32a323edffe9d898995d0eb652f06ccb09d9
robert patterson <rob@robpatterson.net>
2021-10-14 15:31:47 -0700

    more

---
+++ src/Home.js
@@ -0,0 +1,29 @@
+import fly from './butterfly.gif';
+import bg from './bg.jpg';
+import b2 from './b2.jpg';
+import b3 from './b3.jpg';
+
+
+function Home() {
+    return (
+      <div className="Home">
+
+       <>
+      <img src={bg} width="100%" margin-top="1px" />
+      <img src={b2} width="100%" margin-top="1px" />
+
+
+      <img src={b3} width="100%" margin-top="1px" />
+
+
+
+       </>
+
+
+
+      </div>
+    );
+  }
+  
+  export default Home;
+  
